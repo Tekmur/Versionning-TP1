0e60d4417209c867ad07e1384f745947bff20ead
Remy Noel <noel.remy@gmail.com>
2016-03-30 12:23:32 +0200

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,7 @@
 ###application.bat - 2016 - Noel Remy###
+version = "1.2"
+build_hour="11H00"
 echo "Copyright company - 2016"
-echo "Build : 10:00"
-echo "version 1.1"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
